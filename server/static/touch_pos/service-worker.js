ab2db440bb595048d39e6960a1c43d21813fda21
focuseyes1989 <focuseyes1989@gmail.com>
2026-09-09 19:51:33 +0630

    Dim and blur page backgrounds behind Touch dialogs

--- server/static/touch_pos/service-worker.js
+++ server/static/touch_pos/service-worker.js
@@ -1,12 +1,12 @@
-const CACHE = 'kay-pos-touch-w7-v97';
+const CACHE = 'kay-pos-touch-w7-v98';
 const SHELL = [
-  '/static/touch_pos/touch-scanner.js?v=20260909-quick-received1',
+  '/static/touch_pos/touch-scanner.js?v=20260909-dialog-backdrop1',
   '/touch-pos/',
-  '/static/touch_pos/touch-pos.css?v=20260909-quick-received1',
-  '/static/touch_pos/touch-pos.js?v=20260909-quick-received1',
-  '/static/touch_pos/touch-settings.js?v=20260909-quick-received1',
-  '/static/touch_pos/touch-receipt.js?v=20260909-quick-received1',
-  '/static/touch_pos/touch-expenses.js?v=20260909-quick-received1',
+  '/static/touch_pos/touch-pos.css?v=20260909-dialog-backdrop1',
+  '/static/touch_pos/touch-pos.js?v=20260909-dialog-backdrop1',
+  '/static/touch_pos/touch-settings.js?v=20260909-dialog-backdrop1',
+  '/static/touch_pos/touch-receipt.js?v=20260909-dialog-backdrop1',
+  '/static/touch_pos/touch-expenses.js?v=20260909-dialog-backdrop1',
   '/static/touch_pos/manifest.webmanifest',
   '/assets/kay/kay_128x128.png',
   '/assets/icons/dashboard.svg',
