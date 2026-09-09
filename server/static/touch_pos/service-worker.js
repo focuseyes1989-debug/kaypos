4cec07bb2600a01ecb44f1d4f5cf69530a7553dc
focuseyes1989 <focuseyes1989@gmail.com>
2026-09-09 14:49:58 +0630

    Use two-column expense editor and consistent Touch action heights

--- server/static/touch_pos/service-worker.js
+++ server/static/touch_pos/service-worker.js
@@ -1,10 +1,10 @@
-const CACHE = 'kay-pos-touch-w7-v72';
+const CACHE = 'kay-pos-touch-w7-v73';
 const SHELL = [
   '/touch-pos/',
-  '/static/touch_pos/touch-pos.css?v=20260909-expense-filters1',
-  '/static/touch_pos/touch-pos.js?v=20260909-expense-filters1',
-  '/static/touch_pos/touch-settings.js?v=20260909-expense-filters1',
-  '/static/touch_pos/touch-receipt.js?v=20260909-expense-filters1',
+  '/static/touch_pos/touch-pos.css?v=20260909-action-layout1',
+  '/static/touch_pos/touch-pos.js?v=20260909-action-layout1',
+  '/static/touch_pos/touch-settings.js?v=20260909-action-layout1',
+  '/static/touch_pos/touch-receipt.js?v=20260909-action-layout1',
   '/static/touch_pos/touch-expenses.js',
   '/static/touch_pos/manifest.webmanifest',
   '/assets/kay/kay_128x128.png',
