e1d7fd5f6835e33a269f096f8a5c490abac5ff3d
focuseyes1989 <focuseyes1989@gmail.com>
2026-09-09 15:46:31 +0630

    Compact product rows and place type badges beside names

--- server/static/touch_pos/service-worker.js
+++ server/static/touch_pos/service-worker.js
@@ -1,10 +1,10 @@
-const CACHE = 'kay-pos-touch-w7-v78';
+const CACHE = 'kay-pos-touch-w7-v79';
 const SHELL = [
   '/touch-pos/',
-  '/static/touch_pos/touch-pos.css?v=20260909-products-ui1',
-  '/static/touch_pos/touch-pos.js?v=20260909-products-ui1',
-  '/static/touch_pos/touch-settings.js?v=20260909-products-ui1',
-  '/static/touch_pos/touch-receipt.js?v=20260909-products-ui1',
+  '/static/touch_pos/touch-pos.css?v=20260909-products-compact1',
+  '/static/touch_pos/touch-pos.js?v=20260909-products-compact1',
+  '/static/touch_pos/touch-settings.js?v=20260909-products-compact1',
+  '/static/touch_pos/touch-receipt.js?v=20260909-products-compact1',
   '/static/touch_pos/touch-expenses.js',
   '/static/touch_pos/manifest.webmanifest',
   '/assets/kay/kay_128x128.png',
